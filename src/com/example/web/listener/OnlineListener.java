60214b5eb835ecf84fca1548699f0e2984b1c911
lghuntfor <lghuntfor@sina.com>
2015-06-13 20:23:41 +0800

    online

---
+++ src/com/example/web/listener/OnlineListener.java
@@ -0,0 +1,43 @@
+package com.example.web.listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * 在线用户监听器
+ */
+public class OnlineListener implements HttpSessionAttributeListener {
+	public static Map<Object, OnlineModel> onlines = new HashMap<>();
+	
+    public OnlineListener() {
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent se)  { 
+    	
+    }
+
+    public void attributeAdded(HttpSessionBindingEvent se)  { 
+    	if("user".equals(se.getName())) {
+    		OnlineModel model = (OnlineModel) se.getValue();
+    		Object userId = model.getUserId();
+    		if(!onlines.containsKey(userId)) {
+    			onlines.put(userId, model);
+    		}
+    	}
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent se)  { 
+    	
+    }
+	
+    public void contextInitialized(ServletContextEvent sce) {
+		ServletContext application = sce.getServletContext();
+		application.setAttribute("onlines", onlines);
+	}
+    
+}
